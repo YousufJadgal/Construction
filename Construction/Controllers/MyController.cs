1ff1c0731650e9a6b15a80062700e77b75d8b22a
Administrator <Administrator@PC15-LAB1>
2023-08-28 18:47:50 +0500

    Add project files.

---
+++ Construction/Controllers/MyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Construction.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+        }
+    }
+}
